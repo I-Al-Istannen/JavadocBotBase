d096d47be80e34fa6e5968a3338fd1a0ff0d82da
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2017-04-03 16:14:14 +0200

    Added an IconUtil to get icons for methods and classes

--- src/main/java/me/ialistannen/javadocbot/util/IconUtil.java
+++ src/main/java/me/ialistannen/javadocbot/util/IconUtil.java
@@ -1,62 +1,45 @@
-import me.ialistannen.javadocbot.javadoc.model.*;
+package me.ialistannen.javadocbot.util;
 
-public class JavadocUtil {
+import me.ialistannen.javadocbot.javadoc.model.JavadocClass;
+import me.ialistannen.javadocbot.javadoc.model.JavadocMethod;
 
-  /**
-   * Trims a String to a given size
-   *
-   * @param input The input String
-   * @param max The max size
-   * @return The trimmed String, or the original if it was small enough
-   */
-  private static String trimToSize(String input, int max) {
-    if (input.length() < max) {
-      return input;
-    }
-    return input.substring(0, max - 3) + "...";
-  }
+public class IconUtil {
 
   /**
-   * Strips all formatting from the String
-   *
-   * @param input The input String
-   * @return The String without any formatting
-   */
-  private static String stripFormatting(String input) {
-    String strippedContent = input.replaceAll("[*`_~]", "");
-    return strippedContent.replaceAll("\\[(.+?)]\\(.+?\\)", "$1");
-  }
-  
-   /**
-   * Gets the icon URL for a specified java class
+   * Gets the icon URL for a specified java class.
+   * <p>
+   * Distinguishes between different types of classes, e.g. Enums or Interfaces.
+   * <br>Shamelessly links jetbrains icons.
    *
    * @param javadocClass The java class.
    * @return The icon url for the specific class
    */
-  private String getIconUrlForClass(JavadocClass javadocClass) {
+  public static String getIconUrlForClass(JavadocClass javadocClass) {
     if (javadocClass.getDeclaration().contains("abstract")) {
-      return "https://www.jetbrains.com/help/img/idea/2016.3/classTypeAbstract.png";
+      return "https://www.jetbrains.com/help/img/idea/2017.1/classTypeAbstract.png";
     } else if (javadocClass.getType().equalsIgnoreCase("interface")) {
-      return "https://www.jetbrains.com/help/img/idea/2016.3/classTypeInterface.png";
+      return "https://www.jetbrains.com/help/img/idea/2017.1/classTypeInterface.png";
     } else if (javadocClass.getType().equalsIgnoreCase("enum")) {
-      return "https://www.jetbrains.com/help/img/idea/2016.3/classTypeEnum.png";
+      return "https://www.jetbrains.com/help/img/idea/2017.1/classTypeEnum.png";
     } else if (javadocClass.getDeclaration().contains("final")) {
-      return "https://www.jetbrains.com/help/img/idea/2016.3/classTypeFinal.png";
+      return "https://www.jetbrains.com/help/img/idea/2017.1/classTypeFinal.png";
     }
-    return "https://www.jetbrains.com/help/img/idea/2016.3/classTypeJavaClass.png";
-  }
-  
-   /**
-   * Gets the icon URL for a specified java method
-   *
-   * @param javadocClass The java method.
-   * @return The icon url for the specific method
-   */
-  private String getIconForMethod(JavadocMethod method) {
-    if (method.getDeclaration().contains("abstract")) {
-      return "https://www.jetbrains.com/help/img/idea/2016.3/method_abstract.png";
-    }
-    return "https://www.jetbrains.com/help/img/idea/2016.3/method.png";
+    return "https://www.jetbrains.com/help/img/idea/2017.1/classTypeJavaClass.png";
   }
 
+  /**
+   * Gets the icon URL for a specified java method.
+   * <p>
+   * Distinguishes abstract and non abstract methods.
+   * <br>Shamelessly links jetbrains icons.
+   *
+   * @param method The java method.
+   * @return The icon url for the specific method
+   */
+  public static String getIconForMethod(JavadocMethod method) {
+    if (method.getDeclaration().contains("abstract")) {
+      return "https://www.jetbrains.com/help/img/idea/2017.1/method_abstract.png";
+    }
+    return "https://www.jetbrains.com/help/img/idea/2017.1/method.png";
+  }
 }
